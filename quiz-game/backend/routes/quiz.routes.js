1d9be3152d179f48714ae97d1af8e9389623dd97
RachlGew <wege8390@uni.sydney.edu.au>
2026-04-30 21:14:19 +1000

    deleted main contents
    add new branches

--- quiz-game/backend/routes/quiz.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { startQuiz, submitQuiz } = require('../controllers/quiz.controller');
-
-router.get('/start', startQuiz);
-router.post('/submit', submitQuiz);
-
-module.exports = router;
